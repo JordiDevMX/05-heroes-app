e3c1190fdaab687d92ae21e0513b680ed499a9ac
Jordan Garcia <jordidev@proton.me>
2026-01-11 13:56:19 -0600

    Lazy Load - Carga perezosa

--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -1,8 +1,22 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 // TODO: Crear un admin layout de diferente color
 export const HeroesLayout = () => {
   return (
-    <div className="bg-red-500">
+    <div className="bg-red-100">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/heroes/1">Hero</Link>
+        </li>
+        <li>
+          <Link to="/search">Busqueda</Link>
+        </li>
+        <li>
+          <Link to="/admin">Admin</Link>
+        </li>
+      </ul>
       {/* Esto hace que todas las rutas hijas tengan este mismo contenedor */}
       <Outlet />
     </div>
--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,11 +1,22 @@
 import { createBrowserRouter } from "react-router";
+import { lazy } from "react";
 
 import { HomePage } from "@/heroes/pages/home/HomePage";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
 import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { AdminLayout } from "@/admin/layouts/AdminLayout";
+
+// import { SearchPage } from "@/heroes/pages/search/SearchPage";
+
+// ! Codigo legacy: no usar
+// const SearchPage = lazy(() =>
+//   import("@/heroes/pages/search/SearchPage").then((module) => ({
+//     default: module.SearchPage,
+//   }))
+// );
+
+const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
 export const appRouter = createBrowserRouter([
   {
