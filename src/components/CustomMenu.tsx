8ebebc5858de6d8bc02c35226270793c00878ed6
JordiDev <jordidev@proton.me>
2026-01-17 23:00:41 -0600

    Menú superior - Identificar ruta activa

---
+++ src/components/CustomMenu.tsx
@@ -0,0 +1,41 @@
+import {
+  NavigationMenuItem,
+  NavigationMenuLink,
+} from "@radix-ui/react-navigation-menu";
+import { Link, useLocation } from "react-router";
+import { NavigationMenuList, NavigationMenu } from "./ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+export const CustomMenu = () => {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => pathname === path;
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {/* Home */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            asChild
+            className={cn(isActive("/") && "bg-slate-200 rounded-md", "p-2")}
+          >
+            <Link to="/">Home</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        {/* Search */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            asChild
+            className={cn(
+              isActive("/search") && "bg-slate-200 rounded-md",
+              "p-2",
+            )}
+          >
+            <Link to="/search">Buscar Personajes</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
